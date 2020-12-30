6fa7886d484e5756bb948d7b9fffc48b833ea419
Mohammad Ilham Nurdhi Hafsyah <ilhamhafsyah15@gmail.com>
2020-12-30 14:39:53 +0700

    added MONTHNAME for per month income

--- src/model/m_dashboard.js
+++ src/model/m_dashboard.js
@@ -35,7 +35,7 @@ module.exports = {
     return new Promise((resolve, reject) => {
       connection.query(
         // `SELECT MONTHNAME(history_created_at) FROM history GROUP BY MONTH(history_created_at)`,
-        'SELECT SUM(subtotal) AS per_month_income FROM history WHERE YEAR(history_created_at) = YEAR(NOW()) GROUP BY MONTH(history_created_at)',
+        'SELECT MONTHNAME(history_created_at) AS month, SUM(subtotal) AS per_month_income FROM history WHERE YEAR(history_created_at) = YEAR(NOW()) GROUP BY MONTH(history_created_at)',
         (error, result) => {
           !error ? resolve(result) : reject(new Error(error))
         }
