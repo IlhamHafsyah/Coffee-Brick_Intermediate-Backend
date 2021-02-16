d9b94ab1b6b456a2d0ca9a0d612112f56bcd9c68
Mohammad Ilham Nurdhi Hafsyah <ilhamhafsyah15@gmail.com>
2021-02-16 13:13:19 +0700

    add express static config

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const routesNavigation = require('./src/routesNavigation')
 const app = express()
 app.use(cors())
 app.use(morgan('dev'))
-app.use(express.static('upload'))
+app.use('/api2/fileUploadsApi2', express.static('upload'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 // ====================
@@ -24,7 +24,7 @@ app.use((request, response, next) => {
 })
 // ====================
 
-app.use('/', routesNavigation)
+app.use('/api2', routesNavigation)
 
 app.get('*', (request, response) => {
   response.status(404).send('Path not found !')
